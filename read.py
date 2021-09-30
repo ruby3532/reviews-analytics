3c228431cc8b8ce5b08e8ac9fa24cbf145d565fb
李心汝 <lixinru@lixinrudeAir.Home>
2021-09-30 22:03:13 +0800

    add average lenth

--- read.py
+++ read.py
@@ -1,10 +1,24 @@
 data = []
+count = 0
 with open('reviews.txt' , 'r') as f:
 	for line in f:
 		data.append(line)
-		print(len(data))
-print(len(data))
+		count += 1 
+		if count % 1000 == 0: # % 是用來求餘數
+			print(len(data))  # 了解讀取進度
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print(data[0]) # 印第一筆
-print(-------------------)
-print(data[1])
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言的平均長度為' , sum_len/ len(data))
+
+	# print('平均長度為' , float(sum_len[-1]) / 1000)
+	# print(sum_len)
+
+
+#print(data[0]) # 印第一筆
+#print(-------------------)
+#print(data[1])
